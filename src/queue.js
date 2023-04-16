be87145829a50bc52452410068a445db5632b2a4
marina-eckert <mar2001aap@gmail.com>
2023-04-16 22:34:08 +0300

    feat: add solution

--- src/queue.js
+++ src/queue.js
@@ -15,19 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.elements = {};
+    this.tail = 0;
+    this.head = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return newQueue;
+  }
+
+
+  enqueue(value) {
+    this.elements[this.tail] = value;
+    this.tail++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const item = this.elements[this.head];
+    delete this.elements[this.head];
+    this.head++;
+    return item;
   }
 }
 
